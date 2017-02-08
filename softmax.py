7df9bfa280b557a887198f47818a6a425d95dbec
llbehappy <hao.d.yang@gmail.com>
2017-02-07 23:34:19 -0500

    softmax

---
+++ softmax.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+scores = [[3.0, 1.0, 2.0],
+          [1.3, 4.0, 2.7]]
+
+
+def softmax(x):
+    """
+    Purpose: compute softmax value for x
+    :param x:
+    :return:
+    """
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+print softmax(scores)
+
+# plot
+import matplotlib.pyplot as plt
+
+
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+print scores
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
