4f4e82d9345979bd1dea15cd49349466663276ea
llbehappy <hao.d.yang@gmail.com>
2017-02-13 22:20:36 -0500

    Upload assignment solutions.

--- softmax.py
+++ softmax.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from __future__ import print_function
+
 
 scores = [[3.0, 1.0, 2.0],
           [1.3, 4.0, 2.7]]
@@ -12,14 +15,12 @@ def softmax(x):
     """
     return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-print softmax(scores)
+print(softmax(scores))
 
 # plot
-import matplotlib.pyplot as plt
-
 
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
-print scores
+print(scores)
 plt.plot(x, softmax(scores).T, linewidth=2)
 plt.show()
